3bd306b341e84946071f6c2bf8f50c868547586c
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2022-12-29 02:12:43 +0530

    pipe operator in TS and unions

--- 19_unions_pipe_operator/pipe_operator.js
+++ 19_unions_pipe_operator/pipe_operator.js
@@ -9,3 +9,8 @@ function fun(id) {
     }
 }
 fun("harshit");
+function gun(s) {
+    return s.slice(1, 3);
+}
+console.log(gun("harshit"));
+console.log(gun([10, 20, 30, 40]));
--- 19_unions_pipe_operator/pipe_operator.ts
+++ 19_unions_pipe_operator/pipe_operator.ts
@@ -14,3 +14,12 @@ function fun(id:Id) {
 }
 
 fun("harshit");
+
+
+// we use pipe operator to expects type as string or number[]
+function gun(s:string | number[]) {
+    return s.slice(1,3);
+}
+
+console.log(gun("harshit"));
+console.log(gun([10,20,30,40]));
