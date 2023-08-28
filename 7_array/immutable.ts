e02208b33a8ddc5dcdc45677435707f16f77eb95
Harshit Raj <harshit.raj@itential.com>
2023-08-29 00:35:53 +0530

    updated TS

--- 7_array/immutable.ts
+++ 7_array/immutable.ts
@@ -3,22 +3,21 @@
 let list: ReadonlyArray<number> = [1, 2]; // since it's read only array, we can push and pop elements
 console.log(list);
 
-
 // another way
 let list1: readonly number[] = [1, 2];
 console.log(list1);
 
+let l1: readonly string[] = ["1", "2", "3"];
+// l1.push("harshit");
 
-// What is the difference between a constant array and a read-only array?” 
-// The answer is that a constant array won’t let you assign values to a list 
+// What is the difference between a constant array and a read-only array?”
+// The answer is that a constant array won’t let you assign values to a list
 // while a read-only array blocks you from changing values.
 
-const kl:number[] = [20,40];
+const kl: number[] = [20, 40];
 kl[2] = 60; // it'll allow to add some values but readonlyArray doesn't
 console.log(kl);
 
-
-
 // ----> best way
-const lp: readonly number[] = [100,200];
+const lp: readonly number[] = [100, 200];
 console.log(lp);
